d5a9eae7cebbffbf95b1c50a736c5f2146703d68
Phryxia <xahhaepica@gmail.com>
2026-02-02 02:04:11 +0900

    refactor: remove unused dotenv

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import dotenv from 'dotenv'
 import { resolve } from 'path'
 import { tanstackRouter } from '@tanstack/router-plugin/vite'
-
-dotenv.config({
-  path: '../../.env',
-})
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -22,7 +17,6 @@ export default defineConfig({
       usePolling: true,
     },
     open: true,
-    port: Number(process.env.CLIENT_PORT),
     fs: {
       allow: ['..'],
     },
